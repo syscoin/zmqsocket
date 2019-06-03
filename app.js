2623c07cdd8bbc27c6bf5440a6aed0e82fe846fa
Dan Wasyluk <Dan Wasyluk 987186+dwasyluk@users.noreply.github.com>
2019-06-02 21:02:05 -0400

    better handler handling

--- app.js
+++ app.js
@@ -20,22 +20,28 @@ sock.subscribe('hashtx');
 
 // create websocket server
 const echo = sockjs.createServer({ prefix:'/zmq' });
+let clientConn;
 
 // setup websocket
 echo.on('connection', function(conn) {
   console.log("client connected");
-
-  sock.on('message', function(topic, message) {
-    //console.log('[raw] TOPIC:', topic, ' MESSAGE', message);
-    console.log('[->client] TOPIC:', topic.toString('utf8'), ' MESSAGE', message.toString('hex'));
-
-    conn.write({ topic: topic.toString('utf8'), message: message.toString('hex')});
-  });
+  clientConn = conn;
 
   conn.on('close', function() {
     console.log("client disconnected");
   });
 });
+
+sock.on('message', handleMessage);
+
+function handleMessage(topic, message) {
+  if(clientConn) {
+    //console.log('[raw] TOPIC:', topic, ' MESSAGE', message);
+    console.log('[->client] TOPIC:', topic.toString('utf8'), ' MESSAGE', message.toString('hex'));
+
+    clientConn.write(JSON.stringify({topic: topic.toString('utf8'), message: message.toString('hex')}));
+  }
+}
 
 const server = http.createServer();
 echo.installHandlers(server);
