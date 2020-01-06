73f32bd4ae1ad662f629dd20288ba873334334ce
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-01-06 15:08:49 -0500

    update config

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ dotenv.config({ silent: true });
 
 module.exports = {
   zmq_address: process.env.ZMQ_ADDRESS || 'tcp://127.0.0.1:28332',
-  ws_port: process.env.WA_PORT || 9999,
+  ws_port: process.env.WS_PORT || 9999,
 };
